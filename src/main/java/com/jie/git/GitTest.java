713b457708ae7350fc1c49c019ecb75489c8d402
jiesheng88 <1049272251@qq.com>
2022-06-20 20:01:34 +0800

    second commit

---
+++ src/main/java/com/jie/git/GitTest.java
@@ -0,0 +1,18 @@
+package com.jie.git;
+
+/**
+ * @author jie
+ * @date 2021-10-27 19:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git！");
+        System.out.println("hello Git2！");
+        System.out.println("hello Git3！");
+        System.out.println("hello Git4！");
+        System.out.println("master test！");
+        System.out.println("hot-fix test！");
+        System.out.println("push test！");
+        System.out.println("pull test！");
+    }
+}
